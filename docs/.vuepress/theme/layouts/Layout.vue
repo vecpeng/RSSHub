764ce3d6a4633000537532160dd49ff0e03cccf8
vecpeng <841298391cp@gmail.com>
2022-11-09 20:31:49 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <ParentLayout>
+    <template #sidebar-top>
+      <CarbonAds />
+    </template>
+    <template #page-bottom>
+      <div class="wwads-cn wwads-vertical wwads-sticky" data-id="193" style="max-width:180px"></div>
+    </template>
+  </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from '@parent-theme/layouts/Layout.vue'
+import CarbonAds from '@theme/components/CarbonAds.vue'
+
+export default {
+  name: 'Layout',
+
+  components: {
+    ParentLayout,
+    CarbonAds,
+  }
+}
+</script>
